30d1013674436ae20e5dddd9e11ba609e5fed9cd
sapnapawar <sapna.pawar31@gmail.com>
2021-01-28 08:52:30 -0600

    Revised with jQuery

--- PizzaCostCalc.js
+++ PizzaCostCalc.js
@@ -10,11 +10,24 @@
 // Divide the total price of pizza by number of coworkers
 // Round the number to 2 digits
 
+$(document).ready(
+    function() {
+        // find the button with jquery
+
+        // assign function calculateTrip to run when the button is clicked
+
+        $("button").click(pizzaPrice);
+    }
+);
+
+
 function pizzaPrice()
 {
-    let numPizza = parseInt(prompt("Number of Pizza"));
+    //let numPizza = parseInt(prompt("Number of Pizza"));
+    let numPizza = parseInt( $("input#numPizza").val() );
 
-    let numTopping = parseInt(prompt("Number of toppings"));
+    //let numTopping = parseInt(prompt("Number of toppings"));
+    let numTopping = parseInt( $("input#numToppings").val() );
 
     let basicPizzaCost = 15.00;
 
@@ -22,14 +35,19 @@ function pizzaPrice()
 
     let totalPizzaCost = numPizza * basicPizzaCost + numTopping * toppingPrice;
 
-    alert(`Total Cost of Pizza ${totalPizzaCost}`)
+    let displayDollars = totalPizzaCost.toFixed(2);
 
-    let numCoworker = parseInt(prompt("Number of Coworkers"));
+    //let numCoworker = parseInt(prompt("Number of Coworkers"));
+    let numCoworker = parseInt( $("input#numCoworker").val() );
 
     let splitPrice = totalPizzaCost / numCoworker;
 
     let displayAmount = splitPrice.toFixed(2);
 
-    alert(`$${displayAmount} per person `)
+    $("p#totalPizzaCost").text(`The total cost of Pizza is $${displayDollars}`);
+
+    // alert time
+    // alert();
+    $("p#splitPrice").text(`You each will pay $${displayAmount}`);
 }
 
